4eea4ed410566c942cdac151579183a78300997e
STAR-Tournaments <tournaments.star@gmail.com>
2021-09-29 14:54:32 -0300

    package-json changes

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const path = require('path');
-const app = express()
+const app = express();
+
+// settings
+
+const hostname = 'tomascoffe.github.io';
+const port = 8000;
+app.use(express.static(path.join(__dirname, 'views')));
+
+// server listen
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'));
   console.log('Petición web')
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
